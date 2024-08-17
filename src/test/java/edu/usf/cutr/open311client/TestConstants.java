bb7f63901032c8da1b17465086a7f9dd011344e0
Amr Hossam <amrhossam.dev@gmail.com>
2024-08-17 04:30:38 +0300

    Update test end-point url

--- src/test/java/edu/usf/cutr/open311client/TestConstants.java
+++ src/test/java/edu/usf/cutr/open311client/TestConstants.java
@@ -19,5 +19,5 @@ public class TestConstants {
 
   public static final String API_KEY = "937033cad3054ec58a1a8156dcdd6ad8a416af2f";
   
-  public static final String ENDPOINT = "http://test.seeclickfix.com/open311/v2/";
+  public static final String ENDPOINT = "https://seeclickfix.com/open311/v2/29/";
 }
